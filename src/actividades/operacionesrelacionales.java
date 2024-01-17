73675779706813a04c0b1eca7a7bd62c45d1c419
fjavier72 <fjavierp72@gmail.com>
2024-01-17 17:07:54 +0100

    subida de archivos

---
+++ src/actividades/operacionesrelacionales.java
@@ -0,0 +1,11 @@
+package actividades;
+
+public class operacionesrelacionales {
+    public static void main(String[] args) {
+
+    boolean primeraEv=5<=3;
+    System.out.println ("5<3- "+ primeraEv);
+
+boolean segundaEv=6!= 2;
+    }
+}
