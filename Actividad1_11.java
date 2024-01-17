a27479f55dae451fcc40772e2b2c513e6c5ef52a
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-01-17 20:41:23 +0100

    Add files via upload

---
+++ Actividad1_11.java
@@ -0,0 +1,21 @@
+package actividades; //Un frutero necesesita calcular los benefricios anuales que obtiene
+//de la ventade manzanas y peras. por este motivo, es necesario deseñar una aplicación
+//que solicite las ventas ( en kilos) de cada semestre para cada fruta. La aplicación mostrará el importe
+//total sabiendo que el precio del kilo de manzanas está fijado en 2,35€ y de peras en 1,95€
+//
+
+
+public class Actividad1_11 {
+    public static void main(String[] args) {
+
+        //*Los datos de entrada que necesitamos son:
+        // * Cantidad vendida en el semestre 1 ( de peras y manzanas)
+        //* Cantidad vendida en el semestre 2 (idem) */
+
+
+    }
+
+
+
+
+}
