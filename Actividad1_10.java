a30fa59ad4f70f8db53e21e0df92da776859ae5c
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-01-17 20:41:09 +0100

    Add files via upload

---
+++ Actividad1_10.java
@@ -0,0 +1,24 @@
+package actividades;
+/*/ Diseñar un algoritmo que nos indique si podemos salir a a la calle. Existen
+* aspectos que influiran ne esta decisión: si está lloviendo y si hemos terminado
+* nuestras tareas. Solo podremos salir a la calle si no está lloviendo y hemos finalizado nuestras tareas.
+* Existe una opción en la que, indistintamente de lo anterior, podremos salir a la calle: el hecho de
+* que tengamos que que ir a la biblioteca (para realizar algún trabajo, entregar un libro, etc.)
+* Solicitar al usuario (mediante un booleano) si llueve, si ha finalizado las tareas y si necesita ir a la biblioteca
+* El algoritmo debe mostrar mediante un booleano (true o false) si es posible que se le otorgue permiso para ir a la
+* calle.   */
+
+import java.util.Scanner;
+
+public class Actividad1_10 {
+    public static void main(String[] args) {
+    //Tras solicitar la informacion requerida uaremos operadores
+    //lógicos para conseguir saber si es posible salir a la calle.
+        boolean llueve, finalizadoTareas, irBiblioteca;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Esta lloviendo (true o false)");
+
+
+
+    }
+}
