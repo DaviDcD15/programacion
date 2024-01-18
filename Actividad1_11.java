48645d556d9fdc00caa224fce780ae0cf183dfb8
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-01-18 17:28:45 +0100

    Add files via upload

--- Actividad1_11.java
+++ Actividad1_11.java
@@ -5,17 +5,43 @@ package actividades; //Un frutero necesesita calcular los benefricios anuales qu
 //
 
 
+import jdk.jshell.ImportSnippet;
+
+import java.util.Scanner;
+
 public class Actividad1_11 {
+    //*Los datos de entrada que necesitamos son:
+    // * Cantidad vendida en el semestre 1 ( de peras y manzanas)
+    //* Cantidad vendida en el semestre 2 (idem) */
     public static void main(String[] args) {
-
-        //*Los datos de entrada que necesitamos son:
-        // * Cantidad vendida en el semestre 1 ( de peras y manzanas)
-        //* Cantidad vendida en el semestre 2 (idem) */
-
+        final double PRECIO_MANZANAS= 2.35; //valores constantes, no varian durante
+        //el programa
+        final double PRECIO_PERAS = 1.95;
+        //los identificadores de costantes los escribimos en mayúsculas
+        int vManz1Sem, vManz2Sem; //Ventas (en kilos) por semestre
+        int vPeras1Sem, vPeras2Sem;//igual para las peras
+        double impTotal; //importe total
+        Scanner sc = new Scanner(System.in);
+        //pedimos los datos
+        System.out.println("Para las manzanas: ");
+        System.out.print("Venta (en kilos) del primer semestre: ");
+        vManz1Sem = sc.nextInt();
+        System.out.print("Ventas (en kilos) del segundo semestre: ");
+        vManz2Sem = sc.nextInt();
+        System.out.println("Para las peras: ");
+        System.out.print("Ventas (en kilos) del primer semestre: ");
+        vPeras1Sem = sc.nextInt();
+        System.out.print("Ventas (en kilos) del segundo semestre: ");
+        vPeras2Sem = sc.nextInt();
+        //Calculamos el importe total obtenido
+        impTotal = (vManz1Sem + vPeras2Sem) * PRECIO_MANZANAS;
+        impTotal +=(vPeras1Sem + vPeras2Sem) * PRECIO_PERAS;
+        System.out.println("el importe total es de: " + impTotal + " euros");
+    }
 
     }
 
 
 
 
-}
+
