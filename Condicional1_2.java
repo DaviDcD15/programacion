cce8cddec87fbd76b6a0210b8897dfda2b924948
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-01-24 14:23:56 +0100

    Add files via upload

---
+++ Condicional1_2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Condicional1_2 {
+    public static void main(String[] args) {
+      int opcion, valor1, valor2;
+        Scanner sn=new Scanner(System.in);
+        System.out.println("*********************************");
+        System.out.println("***** Menú de la Calculadora ****");
+        System.out.println("** 1. Sumar                     **");
+        System.out.println("** 2. Restar                    **");
+        System.out.println("** 3. Multiplicar               **");
+}
