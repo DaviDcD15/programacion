4f51f26c82bc8c62d3d4f56cb22461b075a5033d
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-01-24 13:09:39 +0100

    Add files via upload

--- Condicional1_1.java
+++ Condicional1_1.java
@@ -10,5 +10,14 @@ public class Condicional1_1 {
        if (edad<18) System.out.println("no puedes entrar a este pub ni comprar tabaco");
        else System.out.println("debes pagar 5€ para entrar ");
 
+       //if anidados
+       if (edad>18) {
+          System.out.println("entras sin problema");
+       } else if (edad > 5){//mayor que 5 y menor que 18
+          System.out.println("estas en primaria");
+
+       }
+       }
+
 
 }
