5b3d1681751e4abcba4321623491e70581fe2e90
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-03-14 20:43:50 +0100

    Add files via upload

---
+++ Numeros_1_10.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Numeros_1_10{
+
+    public static void main(String[] args) {
+
+        Scanner sn = new Scanner(System.in);
+
+        System.out.println("Pido un número del 1 al 10");
+
+        int numero = sn.nextInt();
+
+        if (numero == 1) {
+            System.out.println("UNO");
+        }
+        if (numero == 2) {
+            System.out.println("DOS");
+        }
+
+            if (numero == 3) {
+                System.out.println("tres");
+            }
+                if (numero == 4) {
+                    System.out.println("CUATRO");
+
+                }
+
+
+
+        }
+    }
