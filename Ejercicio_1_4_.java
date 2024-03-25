8ba84f4144ec8fa939b21b8b8070caece4387738
DaviDcD15 <156712541+DaviDcD15@users.noreply.github.com>
2024-03-25 19:23:47 +0100

    Add files via upload

---
+++ Ejercicio_1_4_.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio_1_4_ {
+
+  public static void main(String[] args) {
+    Scanner sn = new  Scanner (System.in);
+    System.out.println("año de nacimiento");
+    int anio = sn.nextInt();
+        System.out.println("calular la edad - año en curso");
+      int edad=2024-anio;
+
+
+    System.out.println("su edad es " +  edad + " años " );
+  }
+}
+
+
+
+
+
+
